0aff2070d85a6a63ab569788a2b35907296142f7
mrfelfel <qazvinyjavad@gmail.com>
2020-05-21 16:17:23 +0430

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], 
+    originWhitelist: ['https://good.example.com'], 
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
